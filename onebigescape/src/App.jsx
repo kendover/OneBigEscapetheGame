c4eb18550924ca1da71381b5869962f1fb5bd39d
kendover <lylefernandez.071905@gmail.com>
2026-04-19 00:43:22 +0800

    can you please work thank you

---
+++ onebigescape/src/App.jsx
@@ -0,0 +1,10 @@
+import Scene1 from './Scenes/Scene1';
+import './App.css';
+
+function App() {
+  return (
+    <Scene1 />
+  );
+}
+
+export default App;
---
+++ onebigescape/src/scenes/Scene1.jsx
@@ -0,0 +1,25 @@
+import 
+function Scene1( ) {
+    return (
+         <main>
+      <h1> One Big Escape!! </h1>
+      
+          <img src="./graphics/placeholder.png" class="graphic"/> 
+          {/* Image for graphic, please try to make sure it's a jpeg or webm for quicker loading times. */}
+          
+            <p> [PLACEHOLDER] </p>
+          
+          <p> <strong> What do you do? </strong> </p>
+          
+          <ul className="options"> {/* Feel free to add/remove */}
+            <li> <a href=""> LET IT RIP!! </a> </li>
+            <li> <a href=""> Jump Out </a> </li>
+            <li> <a href=""> Go to the bathroom </a> </li>
+            
+          </ul>
+      
+    </main>
+    );
+}
+
+export default Scene1;
